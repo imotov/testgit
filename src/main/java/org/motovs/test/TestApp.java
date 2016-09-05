d31e1d4ae706b5906b33da8392eb95ed951ceab0
Larisa Motova <larisa@motovs.org>
2016-09-05 09:11:08 -0400

    Third commit
    made tiny improvements

--- src/main/java/org/motovs/test/TestApp.java
+++ src/main/java/org/motovs/test/TestApp.java
@@ -7,5 +7,6 @@ public class TestApp {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Huge improvement!!!");
+        System.out.print("tiny improvements");
     }
 }
