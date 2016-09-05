fbd562d904a2d397220a0f8cffa9edd8d2075a30
Igor Motov <igor@motovs.org>
2016-09-05 09:21:17 -0400

    In the testings....

--- src/main/java/org/motovs/test/TestApp.java
+++ src/main/java/org/motovs/test/TestApp.java
@@ -8,5 +8,6 @@ public class TestApp {
         System.out.println("Hello world!");
         System.out.println("Huge improvement!!!");
         System.out.print("tiny improvements");
+        System.out.println("I am in the testing branch!!");
     }
 }
