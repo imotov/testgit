72375a68d3f81d510bc31bd22934b33dba3d6595
Igor Motov <igor@motovs.org>
2016-09-05 08:34:09 -0400

    Initial commit

---
+++ src/main/java/org/motovs/test/TestApp.java
@@ -0,0 +1,10 @@
+package org.motovs.test;
+
+/**
+ * Created by igor on 9/5/16.
+ */
+public class TestApp {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
