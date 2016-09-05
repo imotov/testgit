580193c94a10de19a590f640539e4cd4dc1a92d0
Larisa Motova <larisa@motovs.org>
2016-09-05 09:34:09 -0400

    better commit message 3:

--- src/main/java/org/motovs/test/TestApp.java
+++ src/main/java/org/motovs/test/TestApp.java
@@ -5,7 +5,7 @@ package org.motovs.test;
  */
 public class TestApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!!!!!!!!!");
         System.out.println("Huge improvement!!!");
         System.out.print("tiny improvements");
         System.out.println("I am in the testing branch!!");
